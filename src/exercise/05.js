d28cb5e2c30c856462887e7e509ca1f58ffe2008
Adam Whitehurst <Adam.Whitehurst@live.com>
2020-11-06 14:26:45 -0800

    05

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,27 +1,31 @@
 // useRef and useEffect: DOM interaction
 // http://localhost:3000/isolated/exercise/05.js
 
-import React from 'react'
+import React from 'react';
 // eslint-disable-next-line no-unused-vars
-import VanillaTilt from 'vanilla-tilt'
+import VanillaTilt from 'vanilla-tilt';
 
 function Tilt({children}) {
-  // 🐨 create a ref here with React.useRef()
+  const tiltRef = React.useRef();
 
   // 🐨 add a `React.useEffect` callback here and use VanillaTilt to make your
   // div look fancy.
   // 💰 like this:
-  // const tiltNode = tiltRef.current
-  // VanillaTilt.init(tiltNode, {
-  //   max: 25,
-  //   speed: 400,
-  //   glare: true,
-  //   'max-glare': 0.5,
-  // })
+  React.useEffect(() => {
+    const tiltNode = tiltRef.current
+    VanillaTilt.init(tiltNode, {
+        max: 25,
+        speed: 400,
+        glare: true,
+        scale: 1.1,
+        'max-glare': 0.5,
+        reverse: true,
+      })
+      return () => tiltNode.vanillaTilt.destroy();
+    }, []);
   //
   // 💰 Don't forget to return a cleanup function. VanillaTilt.init will add an
   // object to your DOM node to cleanup:
-  // `return () => tiltNode.vanillaTilt.destroy()`
   //
   // 💰 Don't forget to specify your effect's dependencies array! In our case
   // we know that the tilt node will never change, so make it `[]`. Ask me about
@@ -29,7 +33,7 @@ function Tilt({children}) {
 
   // 🐨 add the `ref` prop to the `tilt-root` div here:
   return (
-    <div className="tilt-root">
+    <div ref={tiltRef} className="tilt-root">
       <div className="tilt-child">{children}</div>
     </div>
   )
